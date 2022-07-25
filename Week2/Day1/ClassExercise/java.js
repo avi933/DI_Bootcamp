0a85e81ea13cb1366fbffed6355395ba32d15f71
avi993 <aviramjeeawon@gmail.com>
2022-07-25 10:22:01 +0400

    starting javascript

---
+++ Week2/Day1/ClassExercise/java.js
@@ -0,0 +1,29 @@
+console.log("hello worold from external file")
+
+let x =3
+let y =5
+let c = x+y
+console.log(c)
+
+let Avishek = "Best student"
+let mika = 99 
+console.log(Avishek)
+console.log(mika + "marks")
+
+//Exercise 1
+
+let addressNumber = "Roches-Noire "
+let addressStreet = "Branch Road "
+let country = "Mauritius"
+
+let globalAddress ="I live at " + addressStreet + addressNumber + country
+console.log(globalAddress)
+
+//Exercise 2
+
+let a = 1993
+let b = 2030
+let e = b - a
+let f = "I will be " + e
+
+console.log(f + " in " + b )
