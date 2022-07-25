51649a067693922db814b47c4bcf4c120459e717
avi993 <aviramjeeawon@gmail.com>
2022-07-25 11:00:24 +0400

    updated class examples

--- Week2/Day1/ClassExercise/java.js
+++ Week2/Day1/ClassExercise/java.js
@@ -27,3 +27,46 @@ let e = b - a
 let f = "I will be " + e
 
 console.log(f + " in " + b )
+
+//Arays Example
+
+    let arrayName = ["Avi", "Mika", "Jayesh"]
+    console.log(arrayName)
+
+//position in arry
+    let colors = ["blue", "yellow", "green", 54]; 
+    let favorite = colors[0];
+    let secondFavorite = colors[2];
+    console.log(favorite)
+    console.log(secondFavorite)
+
+//changing item in an array
+     let g = ["blue", "yellow", "green"]; 
+     g[0] = "pink"
+     console.log(g) 
+//array Mehtods
+    //adding an item in an arry
+        colors.push("orange") 
+        console.log(colors)
+    //removing last item from array
+        colors.pop() 
+        console.log(colors)
+    //taking data from array and convert to string
+        let colorstring = colors.toString() 
+        console.log(colorstring) 
+        console.log(colors) 
+//Exercise3
+    
+        let animal=["cat","dog","fish","rabbit","cow"]
+    //showing the initial array
+    console.log(animal+" :-This is the inital array")
+    //displaying dog from the array
+        console.log(animal[1] +" :-showing array index 1")
+    //replacing rabbit to horse
+         animal[3]="horse"
+         console.log(animal + " :-replacing rabbit to horse in array ")
+    //showing the length of the array
+    console.log(" This array contains: " + animal.length + " items")
+
+
+
