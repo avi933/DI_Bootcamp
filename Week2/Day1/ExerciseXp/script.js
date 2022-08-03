18cc3edae9f6277df8152ba6f701fc7715084293
avi993 <aviramjeeawon@gmail.com>
2022-08-03 12:29:15 +0400

    added hello.txt

--- Week2/Day1/ExerciseXp/script.js
+++ Week2/Day1/ExerciseXp/script.js
@@ -1,4 +1,4 @@
-//Exercise 1
+ //Exercise 1
     let favMeal = ["lunch","breakfast","dinner","souper"]
     let food = ["fried rice","plain rice","briyani","boiled noddles","fried noddles"]
     console.log("I eat " + food[3] +" at every " + favMeal[0])
@@ -23,3 +23,163 @@
 //replacing 1st item in the begining 
      myWatchedSeries[0]="Anupama"
      console.log(myWatchedSeries)
+
+
+//Exercise 3: Temperature convertor
+
+     let Temcl = prompt('Enter temperature in celsius', 20);
+         let a = Temcl
+         let b = (((a/5)*9) +32)
+       alert(`The temperature in celsius is : ${Temcl} and in fahrenheit is : ${b.toFixed(0)} `);
+
+//Exercise 4:
+
+let c;
+    let t = 34;
+    let y = 21;
+
+    console.log(t+y) //first expression
+    // Prediction: 55
+    // Actual: 55
+
+    t = 2;
+
+    console.log(t+y) //second expression
+    // Prediction:23 
+    // Actual: 23
+    console.log(3+4+'5')
+    //prediction:12
+    //actual : 75
+
+  typeof(15)
+// Prediction: number
+// Actual: number
+
+typeof(5.5)
+// Prediction: Number
+// Actual:Number
+
+typeof(NaN)
+// Prediction: Number
+// Actual:Number
+
+typeof("hello")
+// Prediction: string
+// Actual: string
+
+typeof(true)
+// Prediction: boolean
+// Actual: boolean
+
+typeof(1 != 2)
+// Prediction: true
+// Actual: true
+
+"hamburger" + "s"
+// Prediction:hamburgers
+// Actual: hambburgers
+
+"hamburgers" - "s"
+// Prediction:hamburger
+// Actual:hamburger
+
+"1" + "3"
+// Prediction:4
+// Actual:4
+
+"1" - "3"
+// Prediction: -2
+// Actual: -2 
+
+"johnny" + 5
+// Prediction:johnny5
+// Actual: johnny5
+
+"johnny" - 5
+// Prediction:NaN
+// Actual:NaN
+
+99 * "hello"
+// Prediction: NaN
+// Actual:Nan
+
+1 != 1
+// Prediction: false
+// Actual:false
+
+1 == "1"
+// Prediction: true
+// Actual:true
+
+1 === "1"
+// Prediction: false
+// Actual: false
+
+
+//Exercise 6:
+
+
+5 + "34"
+// Prediction: 39
+// Actual: 39
+
+5 - "4"
+// Prediction:1
+// Actual:1
+
+10 % 5
+// Prediction:0
+// Actual:
+
+5 % 10
+// Prediction:5
+// Actual:5
+
+"Java" + "Script"
+// Prediction: JavaScript
+// Actual: JavaScript
+
+" " + " "
+// Prediction: blank
+// Actual:blank
+
+" " + 0
+// Prediction:0
+// Actual:0
+
+true + true
+// Prediction: true
+// Actual:2
+
+true + false
+// Prediction: false
+// Actual:1
+
+false + true
+// Prediction:false
+// Actual: 1
+
+false - true
+// Prediction:-1
+// Actual:-1
+
+!true
+// Prediction:false
+// Actual: false
+
+3 - 4
+// Prediction: -1
+// Actual: -1
+
+"Bob" - "bill"
+// Prediction: NaN
+// Actual:NaN
+
+
+
+
+
+
+
+
+
